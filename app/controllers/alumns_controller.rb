8cba3aec8d53e84181ac9baa11801ee316d82efc
André de Sousa Costa filho <dedaoskater@hotmail.com>
2017-04-05 19:42:14 -0300

    Defines CRUD methods for AlumnsController and CRUD routes to self. Creates alumns index.html.erb view.

--- app/controllers/alumns_controller.rb
+++ app/controllers/alumns_controller.rb
@@ -1,2 +1,35 @@
 class AlumnsController < ApplicationController
+  def index
+    @alumns = Alumn.all
+  end
+
+  def show
+    @alumn = Alumn.find( alumn_params )
+  end
+
+  def new
+    
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+  # Strong params to be passed to a alumn
+  def alumn_params
+
+  end
 end
