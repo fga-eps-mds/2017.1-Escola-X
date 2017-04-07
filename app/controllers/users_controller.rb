cf9358d3f0a7dbc25abee3e5670ba3f696b25fae
FilipeKN4 <filipekn4@gmail.com>
2017-04-07 19:32:33 -0300

    Adds CRUD methods in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,58 @@
+# File name: users_controller.rb
+# Class name: UsersController
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(users_params)
+  end
+
+  def create
+    @user = User.new(users_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if (@user.update(users_params))
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    @user.destroy
+  end
+
+  private
+  # Strong params to be passed to users
+  def users_params
+    @ params.require(:user).permit(:registry,
+                                   :cpf,
+                                   :name,
+                                   :address,
+                                   :phone,
+                                   :gender,
+                                   :permission,
+                                   :birth_date,
+                                   :class,
+                                   :shift,
+                                   :admission_date)
+  end
 end
