d588b37821c6a58adec9d396dc551069f1d6f4e3
guigawiller <guilhermewiller@hotmail.com>
2017-04-12 18:57:53 -0300

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Michael Cera', address: 'rua do ovo'. cpf: '123456789',
+  registry: '123456', phone: '33213030', permission: 'Principal', password: '123456')
+
+User.create(name: 'Michael Douglas', address: 'rua do ovo'. cpf: '40028922',
+    registry: '40028922', phone: '33213030', permission: 'Alumn', password: '123456')
+
+User.create(name: 'Michael Jackson', address: 'rua do ovo'. cpf: '666321',
+      registry: '666321', phone: '33213030', permission: 'Parent', password: '123456')
