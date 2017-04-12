260feddb0d66de39f34f76bfd1301a4f758589f8
FilipeKN4 <filipekn4@gmail.com>
2017-04-11 22:23:10 -0300

    Adds some validations in user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,21 @@
 # Description: Model used to communicate with the table 'users'
 class User < ApplicationRecord
   self.inheritance_column = :permission
-
+  validates :name, presence: { message: "não pode estar em branco" },
+                   length: { minimum: 5,
+                             maximum: 64,
+                             :too_short => "deve possuir no mínimo 5 caracteres",
+                             :too_long => "deve possuir no máximo 64 caracteres" }
+  validates :classroom, presence: { message: "não pode estar em branco" },
+                        length: { minimum: 2,
+                                  maximum: 2,
+                                  :too_short => "deve possuir no mínimo 2 caracteres",
+                                  :too_long => "deve possuir no máximo 2 caracteres" }
+  validates :shift, presence: { message: "não pode estar em branco" },
+                    length: { minimum: 2,
+                              maximum: 2,
+                              :too_short => "deve possuir no mínimo 8 caracteres",
+                              :too_long => "deve possuir no máximo 10 caracteres" }
   def self.permissions
     ['Alumn', 'Principal']
   end
