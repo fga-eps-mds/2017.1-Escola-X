a80cc4747bd31c5dc01cdf34ce5b5e348178bdf1
Nukdown <belchior.emanoel@gmail.com>
2017-04-13 15:57:57 -0300

    Creates destroy method to parents.

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,6 +1,17 @@
 class ParentsController < ApplicationController
 
   def new
-  
+
+  end
+
+  def destroy
+      @parent= Parent.find(params[:id])
+      @parent.destroy
+
+      redirect_to parents_path 
+  end
+
+  def update
+
   end
 end
