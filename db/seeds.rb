6caf179cdcf029fc82be2645fa0fd4725ce7fed8
André de Sousa Costa filho <dedaoskater@hotmail.com>
2017-04-12 22:53:06 -0300

    Adds changes to seeds.rb, adding new approach.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'Michael Cera', address: 'rua do ovo'. cpf: '123456789',
-  registry: '123456', phone: '33213030', permission: 'Principal', password: '123456')
+# User.create(name: 'Michael Cera', address: 'rua do ovo'. cpf: '123456789',
+#   registry: '123456', phone: '33213030', permission: 'Principal', password: '123456')
+#
+# User.create(name: 'Michael Douglas', address: 'rua do ovo'. cpf: '40028922',
+#     registry: '40028922', phone: '33213030', permission: 'Alumn', password: '123456')
+#
+# User.create(name: 'Michael Jackson', address: 'rua do ovo'. cpf: '666321',
+#       registry: '666321', phone: '33213030', permission: 'Parent', password: '123456')
 
-User.create(name: 'Michael Douglas', address: 'rua do ovo'. cpf: '40028922',
-    registry: '40028922', phone: '33213030', permission: 'Alumn', password: '123456')
+initial_population = [
+  ["Michael Cera","rua do ovo","123456789",123456,"33213030","Principal","123456"],
+  ["Michael Douglas","rua do zé","40028922",40028922,"33213030","Alumn","123456"],
+  ["Michael Jackson","rua do limoreiro","30303030",10101001,"33213030","Parent","123456"],
+  ["Solid Snake","hangar bay","101010010",98765432,"33003923","Principal","lalilulelo"]
+]
 
-User.create(name: 'Michael Jackson', address: 'rua do ovo'. cpf: '666321',
-      registry: '666321', phone: '33213030', permission: 'Parent', password: '123456')
+initial_population.each do |name, address, cpf, registry, phone, permission, password|
+  User.create( name: name, address: address, cpf: cpf, registry: registry,
+              phone: phone, permission: permission, password: password )
+end
