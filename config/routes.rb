6dd40bda892939feef5bd2bf4e8184513384c07e
guigawiller <guilhermewiller@hotmail.com>
2017-04-14 11:57:32 -0300

    creates new index, fix Bootstrap and set new route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'parents#new'
+  root 'users#index'
   resources :parents
+  resources :users
 end
