f0ad4d51902405ec9c4fff869751d330602b5347
ViniCarv <vinio.oliveira@gmail.com>
2017-04-14 13:46:50 -0300

    Creates destroy and update methods to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,14 +11,36 @@ end
     @user = User.new
   end
 
+def edit
+  @user = User.find(params[:id])
+end
+
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       redirect_to @user
     else
       render 'new'
+    end
   end
+
+def update
+  @user = User.find(params[:id])
+
+  if @user.update(user_params)
+    redirect_to @users
+  else
+    render 'edit'
+  end
+end
+
+def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+  redirect_to users_path
+end
+
 
 
 private
