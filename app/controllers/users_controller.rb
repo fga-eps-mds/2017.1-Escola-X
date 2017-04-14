a988f722c876a05f5d7be0feaf74041633001a2c
ViniCarv <vinio.oliveira@gmail.com>
2017-04-14 13:05:04 -0300

    create show method to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,22 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+def show
+  @user = User.find(params[:id])
+end
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
   end
 
 
