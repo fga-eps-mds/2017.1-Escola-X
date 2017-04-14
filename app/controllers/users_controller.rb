c04b24176d57f5cfcbfa69088469b6c4bfda4265
ViniCarv <vinio.oliveira@gmail.com>
2017-04-14 12:49:04 -0300

    Creates new and create methods to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+
+private
+def user_params
+  params.require(:user).permit(:name, :address, :phone, :gender)
+end
+
+
+
 end
