95e935d8367a198dddb6631d4f8ef41f8ed8271f
Nukdown <belchior.emanoel@gmail.com>
2017-04-14 11:48:42 -0300

    Fixes multiple migration and some bugs.

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,12 +1,12 @@
-class ParentsController < ApplicationController
 # File name: parents_controller.rb
+# Class name: ParentsController
 # Description: Controller used to communicate with the view highways/show
-#
+class ParentsController < ApplicationController
 
 
-  def new
+##  def new
 
-  end
+##  end
 
   def create
     @user = User.find(params[:user_id])
@@ -22,15 +22,13 @@ class ParentsController < ApplicationController
       redirect_to parents_path
   end
 
-  def update
+##  def update
 
-  end
+##  end
 
 private
-def parent_params
-  params.require(:parent).permit(:parent_cpf)
-end
-
-end
+  def parent_params
+    params.require(:parent).permit(:parent_cpf)
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170414130103) do
+ActiveRecord::Schema.define(version: 20170414135702) do
 
   create_table "parents", force: :cascade do |t|
-    t.string   "cpf_parent"
-    t.string   "string"
+    t.string   "parent_cpf"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_parents_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
