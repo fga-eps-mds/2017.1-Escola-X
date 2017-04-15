79f03d836b0a4128574ee161aa25095c3cc6dc8b
Zero101010 <igotaraujo97@gmail.com>
2017-04-14 21:43:02 -0300

    validates alumn

--- app/models/alumn.rb
+++ app/models/alumn.rb
@@ -1,3 +1,16 @@
 class Alumn < ApplicationRecord
   belongs_to :user
+
+  validates :registry, presence: { message: "não pode estar em branco" },
+                                           length: { minimum: 5,
+                                                     maximum: 6,
+                                                     :too_short => "deve possuir no mínimo 5 caracteres",
+                                                     :too_long => "deve possuir no máximo 6 caracteres" }
+
+  validates :shift, presence: { message: "não pode estar em branco" },
+                                            length: { minimum: 6,
+                                                      maximum: 11,
+                                                      :too_short => "deve possuir no mínimo 6 caracteres",
+                                                      :too_long => "deve possuir no máximo 11 caracteres" }
+
 end
