f2192dbeaedaf0a2e049b84ab598de3e5ba35237
FilipeKN4 <filipekn4@gmail.com>
2017-04-15 10:52:00 -0300

    Changes create alumn form view.

--- app/models/alumn.rb
+++ app/models/alumn.rb
@@ -8,9 +8,9 @@ class Alumn < ApplicationRecord
                                                      :too_long => "deve possuir no máximo 6 caracteres" }
 
   validates :shift, presence: { message: "não pode estar em branco" },
-                                            length: { minimum: 6,
+                                            length: { minimum: 7,
                                                       maximum: 11,
-                                                      :too_short => "deve possuir no mínimo 6 caracteres",
+                                                      :too_short => "deve possuir no mínimo 7 caracteres",
                                                       :too_long => "deve possuir no máximo 11 caracteres" }
 
 end
