e5d75a8f39c5d6ca1de784850b4ab4f56e594b75
FilipeKN4 <filipekn4@gmail.com>
2017-04-15 12:28:49 -0300

    Adds some validations in alumn model and user model. Changes users show view.

--- app/models/alumn.rb
+++ app/models/alumn.rb
@@ -1,16 +1,14 @@
 class Alumn < ApplicationRecord
   belongs_to :user , optional: true
-
+  
   validates :registry, presence: { message: "não pode estar em branco" },
-                                           length: { minimum: 5,
-                                                     maximum: 6,
-                                                     :too_short => "deve possuir no mínimo 5 caracteres",
-                                                     :too_long => "deve possuir no máximo 6 caracteres" }
-
+                       length: { minimum: 5,
+                                 maximum: 6,
+                                 :too_short => "deve possuir no mínimo 5 caracteres",
+                                 :too_long => "deve possuir no máximo 6 caracteres" }
   validates :shift, presence: { message: "não pode estar em branco" },
-                                            length: { minimum: 7,
-                                                      maximum: 11,
-                                                      :too_short => "deve possuir no mínimo 7 caracteres",
-                                                      :too_long => "deve possuir no máximo 11 caracteres" }
-
+                              length: { minimum: 7,
+                                        maximum: 11,
+                                        :too_short => "deve possuir no mínimo 7 caracteres",
+                                        :too_long => "deve possuir no máximo 11 caracteres" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
                              maximum: 64,
                              :too_short => "deve possuir no mínimo 5 caracteres",
                              :too_long => "deve possuir no máximo 64 caracteres" }
+  validates :phone, length: { in: 10..11,
+                              :too_short => "deve possuir no mínimo 10 dígitos",
+                              :too_long => "deve possuir no máximo 11 dígitos" }
 end
