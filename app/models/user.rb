8a85722559355330e6bd1a90b43a8e57acdc79af
Nukdown <belchior.emanoel@gmail.com>
2017-04-15 17:18:54 -0300

    Places a box for errors on form and fixes birth_date validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_one :parent, autosave: true ,dependent: :destroy
 
   accepts_nested_attributes_for :parent
+
+  validates :birth_date, presence: { message: "Não pode estar em branco." }
+
 end
