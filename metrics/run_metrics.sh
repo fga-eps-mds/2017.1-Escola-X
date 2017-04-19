c323e3afd7aba35f8dc8b8a6460e13eef514c545
mattioli <lucas.mattioli@live.com>
2017-04-19 01:31:12 -0300

    Puts .rubocop.yml and creates script for metrics running

---
+++ metrics/run_metrics.sh
@@ -0,0 +1,3 @@
+rubocop --only Metrics/CyclomaticComplexity &> cyclomatic_complexity.txt ..
+rubocop --only Metrics/ClassLength &> class_length.txt ..
+rubocop --only Metrics/AbcSize &> abc_size.txt ..
