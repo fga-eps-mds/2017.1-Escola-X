2ee3d2c050c6da7d14a17dcbfc1a5b2eba7bcf5f
Matheus Pimenta <ultra10vascaino@gmail.com>
2017-05-05 16:46:37 -0300

    "Making tests."
    Signed-off-by: Matheus Pimenta <ultra10vascaino@gmail.com>
    Signed-off-by: Igor Araujo <igotaraujo97@gmail.com>

--- app/models/secretary.rb
+++ app/models/secretary.rb
@@ -4,8 +4,38 @@
 
 class Secretary < Employee
 	belongs_to :employee
+  validates :parent_cpf, :cpf => true
 	has_secure_password
     before_save :validates_password
+
+  validates :shift, presence: { message: "não pode estar em branco" },
+            length: { minimum: 7,
+                      maximum: 11,
+                      :too_short => "deve possuir no mínimo 7 caracteres",
+                      :too_long => "deve possuir no máximo 11 caracteres" }
+
+
+  validates :birth_date, presence: { message: "não pode estar em branco." }
+
+  validates :admision_date, presence: { message: "não pode estar em branco." }
+
+  validates :gender, presence: { message: "Não pode estar em branco." }
+
+  validates :name, presence: { message: "não pode estar em branco" },
+            length: { minimum: 5,
+                      maximum: 64,
+                      :too_short => "deve possuir no mínimo 5 caracteres",
+                      :too_long => "deve possuir no máximo 64 caracteres" }
+
+  validates :address, presence: { message: "não pode estar em branco" },
+            length: { minimum: 5,
+                      maximum: 64,
+                      :too_short => "deve possuir no mínimo 5 caracteres",
+                      :too_long => "deve possuir no máximo 64 caracteres" }
+
+  validates :phone, length: { in: 10..11,
+                              :too_short => "deve possuir no mínimo 10 dígitos",
+                              :too_long => "deve possuir no máximo 11 dígitos" }
 
   before_create{
     generate_token(:authorization_token)
--- spec/controllers/secretary_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe SecretaryController, type: :controller do
-
-end
