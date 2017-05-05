472876f9dc45a2de8c5cf18e330acfbba7c93c3a
mattioli <lucas.mattioli@live.com>
2017-05-05 18:09:53 -0300

    Adds tests for Notification

--- spec/controllers/notifications_controller_spec.rb
+++ spec/controllers/notifications_controller_spec.rb
@@ -1,47 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe NotificationsController, type: :controller do
+  let(:valid_inputs) { { title: "Ausencia de prosso",
+                         notification_text: "vai ter aula nao mlkada",
+                         notification_date: "01/03/1995"} }
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  let(:invalid_inputs) { { title: "prosso vai",
+                           notification_text: "rodaro",
+                           notification_date: "10/03/1993"} }
+
+  describe "Post create" do
+    before(:each) do
+      notification = Notification.create(title: "Ausencia de prosso",
+                                         notification_text: "vai ter aula nao mlkada",
+                                         notification_date: "01/03/1995")
+
+      user = User.create(name: "Michael Cera", phone:"61988885555",
+                         address:"Rua Vida Casa 15,Taguatinga",
+                         password: "12345678", gender:"M",
+                         birth_date:"07/06/1988", permission:"Principal")
+
+      cookies[:authorization_token] = user.authorization_token
+    end
+
+    describe "with valid params" do
+      it "Creates a new Notification" do
+        expect {
+          post :create, params: {:notification => valid_inputs}
+        }.to change(Notification, :count).by 1
+      end
+
+      it "assigns a new notification to @notification" do
+        post :create, params: {:notification => valid_inputs}
+        expect(assigns(:notification)).to be_a Notification
+        expect(assigns(:notification)).to be_persisted
+      end
+
+      it "redirects to @notification page" do
+        post :create, params: {:notification => valid_inputs}
+        expect(response).to redirect_to notification_path(assigns(:notification))
+      end
+    end
+
+    describe "with invalid params" do
+      it "does not create a new Notification" do
+        expect {
+          post :create, params: {:notification => invalid_inputs}
+        }.to change(Notification, :count).by 0
+      end
+
+      it "assigns a new notification but no save to @notification" do
+        post :create, params: {:notification => invalid_inputs}
+        expect(assigns(:notification)).not_to be_persisted
+      end
     end
   end
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-  # 
-  # describe "GET #destroy" do
-  #   it "returns http success" do
-  #     get :destroy
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
