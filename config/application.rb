c29efccc5643893250dc27b187b2f17242f0f4a1
Matheus Pimenta <ultra10vascaino@gmail.com>
2017-05-04 23:19:50 -0300

    "Changing default language."
    Signed-off-by: Matheus Pimenta <ultra10vascaino@gmail.com>
    Signed-off-by: Igor Araújo <igotaraujo97@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,7 @@ module EscolaX
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.default_locale = "pt-BR"
   end
 end
