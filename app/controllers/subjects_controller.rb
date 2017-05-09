49f9cca598935b9fc0fda7fc51203ce576efb934
naiieandrade <andradenaiara@hotmail.com>
2017-05-09 11:18:02 -0300

    "sets method create to subject and its view."
    Signed-off-by: naiieandrade <andradenaiara@hotmail.com>
    matheusbsilva <matheusbattista@hotmail.com>

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,37 @@
 class SubjectsController < ApplicationController
+	include SessionsHelper
   def index
+  	@subjects = Subject.all
+  end
+  def new
+  	if ( is_principal? )
+  		@subject = Subject.new
+  	end
+  end
+  def create
+  	if (is_principal?)
+  		@subject = Subject.new(subject_params)
+  		@teacher = Teacher.find_by_registry(params[:teacher_registry])
+  		@subject.teacher_id = @teacher.id
+
+  		if (@subject.save)
+  			redirect_to @subject 
+  		else
+  			render 'new'
+  		end
+  	end
+  end
+  def destroy
+  	
+  end
+  def edit
+  	
+  end
+  def update
+  	
+  end
+  private
+  def subject_params
+  	params.require(:subject).permit(:name_subject,:class_level,:teacher_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'grades/index'
-
-  get 'subjects/index'
-
   get 'login', to:'sessions#new', as:'login'
   post 'login', to:'sessions#create'
   get 'logout', to:'sessions#destroy', as:'logout'
@@ -23,6 +19,7 @@ Rails.application.routes.draw do
   resources :secretaries
   resources :employees
   resources :teachers
+  resources :subjects
 
   resources :parents do 
   resources :alumns
