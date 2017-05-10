771fb6ffebbfdaee7b13d6150a87d99296c581c1
matheusbsilva <matheusbattista@hotmail.com>
2017-05-10 20:44:05 +0000

    Fixes routes of back button and strikes index.

--- spec/helper_module_spec.rb
+++ spec/helper_module_spec.rb
@@ -8,21 +8,21 @@ module Helpers
     cookies[:authorization_token] = user.authorization_token
   end
   def parent
-    parent = Parent.create!(name:"Jaozin Silva",phone:"61989998888",
+    @parent = Parent.create!(name:"Jaozin Silva",phone:"61989998888",
                            address:"Rua do pao casa 22, Asa sul",
                            password:"12345678", gender:"Masculino",
                            birth_date:"08/10/1989",parent_cpf:"06057577124")
   end
   def alumn
-    alumn = Alumn.create!(name: "Michael Cera", phone:"61988885555",
+    @alumn = Alumn.create!(name: "Michael Cera", phone:"61988885555",
                            address:"Rua Vida Casa 15,Taguatinga",
                            password: "12345678", gender:"M",
-                           birth_date:"07/06/1988", registry:"123456",
+                           birth_date:"07/06/1988", registry:"222951",
                            shift:"matutino",parent_id:parent.id)
   end
 
   def employee
-    employee = Principal.create!(name: "Michael Cera", phone:"61988885555",
+    @employee = Principal.create!(name: "Michael Cera", phone:"61988885555",
                             address:"Rua Vida Casa 15,Taguatinga",
                             password: "12345678", gender:"M",
                             birth_date:"07/06/1988",registry:"123456",
