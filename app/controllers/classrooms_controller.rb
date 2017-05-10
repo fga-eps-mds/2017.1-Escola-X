0fc98df53c5dc04c42056d2dbf612d7f68985823
Nukdown <belchior.emanoel@gmail.com>
2017-05-10 18:38:24 -0300

    Creates destroy and update methods and views for classroom.
    Signed-off-by: Nukdown <belchior.emanoel@gmail.com>
    Signed-off-by: Guigawiller <guilhermewiller@hotmail.com>

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -2,12 +2,20 @@ class ClassroomsController < ApplicationController
   include SessionsHelper
 
   def index
-
+    if ( is_principal? )
+      @classrooms = Classroom.all
+    end
   end
 
   def new
     if (is_principal?)
       @classroom = Classroom.new
+    end
+  end
+
+  def show
+    if ( is_principal? )
+      @classroom = Classroom.find(params[:id])
     end
   end
 
@@ -21,6 +29,32 @@ class ClassroomsController < ApplicationController
       end
     end
   end
+
+  def destroy
+  if ( is_principal? )
+    @classroom = Classroom.find(params[:id])
+    @classroom.destroy
+    redirect_to users_path
+  end
+end
+
+def edit
+  if ( is_principal? )
+    @classroom = Classroom.find(params[:id])
+  end
+end
+
+def update
+  if ( is_principal? )
+    @classroom = Classroom.find(params[:id])
+    if @classroom.update(classroom_params)
+      redirect_to classroom_path(@classroom)
+    else
+      render "classrooms/edit"
+    end
+  end
+end
+
 private
   def classroom_params
     params.require(:classroom).permit(:name_classroom,
