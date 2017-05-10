8595d32f2468dbc49d77711a15ece88c511d2d48
Matheus Pimenta <ultra10vascaino@gmail.com>
2017-05-10 03:02:31 -0300

    Fixed errors registrer's secretary.
    Signed-off-by: Matheus Pimenta <ultra10vascaino@gmail.com>
    Signed-off-by: Igor Araujo <igotaraujo97@gmail.com>

--- app/models/secretary.rb
+++ app/models/secretary.rb
@@ -5,33 +5,43 @@
 class Secretary < Employee
     before_save :validates_password
 
-  validates :shift, presence: { message: "não pode estar em branco" },
+  validates :shift, presence: { message: "não pode estar em branco." },
             length: { minimum: 7,
                       maximum: 11,
-                      :too_short => "deve possuir no mínimo 7 caracteres",
-                      :too_long => "deve possuir no máximo 11 caracteres" }
+                      :too_short => "deve possuir no mínimo 7 caracteres.",
+                      :too_long => "deve possuir no máximo 11 caracteres." }
 
 
   validates :birth_date, presence: { message: "não pode estar em branco." }
 
   validates :admission_date, presence: { message: "não pode estar em branco." }
 
+  validates :registry, presence: {message: "não pode estar em branco."},
+                       length: { in: 5..6,
+                                      :too_short => "deve possuir no mínimo 5 dígitos.",
+                                      :too_long => "deve possuir no máximo 6 dígitos."}
+
   validates :gender, presence: { message: "Não pode estar em branco." }
 
-  validates :name, presence: { message: "não pode estar em branco" },
+  validates :name, presence: { message: "não pode estar em branco." },
             length: { minimum: 5,
                       maximum: 64,
-                      :too_short => "deve possuir no mínimo 5 caracteres",
-                      :too_long => "deve possuir no máximo 64 caracteres" }
+                      :too_short => "deve possuir no mínimo 5 caracteres.",
+                      :too_long => "deve possuir no máximo 64 caracteres." }
 
-  validates :address, presence: { message: "não pode estar em branco" },
+  validates :address, presence: { message: "não pode estar em branco." },
             length: { minimum: 5,
                       maximum: 64,
-                      :too_short => "deve possuir no mínimo 5 caracteres",
-                      :too_long => "deve possuir no máximo 64 caracteres" }
+                      :too_short => "deve possuir no mínimo 5 caracteres.",
+                      :too_long => "deve possuir no máximo 64 caracteres." }
 
   validates :phone, length: { in: 10..11,
-                              :too_short => "deve possuir no mínimo 10 dígitos",
-                              :too_long => "deve possuir no máximo 11 dígitos" }
+                              :too_short => "deve possuir no mínimo 10 dígitos.",
+                              :too_long => "deve possuir no máximo 11 dígitos." }
+
+
+ def self.search(search)
+  where("name LIKE ?", "%#{search}%")
+end
 
 end
