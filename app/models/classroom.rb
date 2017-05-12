7cc21ff82a7708ecb8c7d1c86a61e31d45452902
guigawiller <guilhermewiller@hotmail.com>
2017-05-11 22:36:06 -0300

    Creates tests for Classroom controller
    Signed-off-by: guigawiller <guilhermewiller@hotmail.com>
    Signed-off-by: nukdown <belchior.emanoel@gmail.com>

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -2,4 +2,19 @@ class Classroom < ApplicationRecord
   has_many :alumn
 
   accepts_nested_attributes_for :alumn
+
+  validates :name_classroom, presence: { message: "não pode estar em branco" },
+           length: { minimum: 2,
+                     maximum: 5,
+                     :too_short => "deve possuir no mínimo 2 caracteres",
+                     :too_long => "deve possuir no máximo 5 caracteres" }
+
+  validates :shift_classroom, presence: { message: "não pode estar em branco" },
+           length: { minimum: 7,
+                     maximum: 11,
+                     :too_short => "deve possuir no mínimo 7 caracteres",
+                     :too_long => "deve possuir no máximo 11 caracteres" }
+
+
+
 end
--- spec/controllers/classrooms_controller_spec.rb
+++ spec/controllers/classrooms_controller_spec.rb
@@ -1,5 +1,67 @@
 require 'rails_helper'
 
+RSpec.configure do |c|
+  c.include Helpers
+end
+
 RSpec.describe ClassroomsController, type: :controller do
+
+  let(:valid_inputs) { { name_classroom: "3G", shift_classroom: "matutino" } }
+
+  let(:invalid_inputs) { { name_classroom: "", shift_classroom: "" } }
+
+  describe "GET new" do
+    before(:each) do
+      loggin_principal
+    end
+
+    it "assigns a new classroom as @classroom" do
+      get :new, {}
+      expect(assigns(:classroom)).to be_a_new(Classroom)
+    end
+  end
+
+  describe "POST create" do
+    before(:each) do
+      loggin_principal
+    end
+
+    describe "with valid params" do
+      it "Creates a new Classroom" do
+        expect{
+          post :create, params: {classroom: valid_inputs}
+        }.to change(Classroom, :count).by 1
+      end
+
+      it "assigns a new classroom to Classrooms" do
+        post :create, params: {classroom: valid_inputs}
+        expect(assigns(:classroom)).to be_a Classroom
+        expect(assigns(:classroom)).to be_persisted
+      end
+
+      it "redirects to @classroom page" do
+        post :create, params: {classroom: valid_inputs}
+        expect(response).to redirect_to classroom_path(assigns(:classroom))
+      end
+    end
+
+    describe "with invalid params" do
+      it "does not create a new Classroom" do
+        expect{
+          post :create, params: {classroom: invalid_inputs}
+        }.to change(Classroom, :count).by 0
+      end
+
+      it "assigns a new classroom but no save to Classrooms" do
+        post :create, params: {classroom: invalid_inputs}
+        expect(assigns(:classroom)).not_to be_persisted
+      end
+
+      it "re-render the 'new' template" do
+        post :create, params: {classroom: invalid_inputs}
+        expect(response).to render_template("new")
+      end
+    end
+  end
 
 end
