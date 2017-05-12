f0fe09cbe8e00a805c24d753b46331dcbdb89973
MrVictor42 <guin-hu@hotmail.com>
2017-05-12 17:42:22 -0300

    Teacher edit tests

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -8,6 +8,13 @@ RSpec.describe TeachersController, type: :controller do
                          gender:"M",birth_date:"09/07/1995",
                          password:"12345678",admission_date:"14/04/2012"} }
 
+  let(:valid_inputs_update) { { name:"Bolt",phone:"61983104981",
+                         address:"QR 602 Conjunto 06 Casa 05",
+                         registry:"654321",employee_cpf:"02951294174",
+                         admission_date:"15/04/2012",shift:"Matutino",
+                         gender:"M",birth_date:"09/07/1995",
+                         password:"12345678",admission_date:"14/04/2012"} }
+
   let(:invalid_inputs) { { name:"Nai",phone:"1234",
                            address:"QR 05",
                            registry:"12443",employee_cpf:"77777777",
@@ -79,6 +86,44 @@ RSpec.describe TeachersController, type: :controller do
       it "re-render the 'new' template" do
         post :create, params: {teacher: invalid_inputs}
         expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    before(:each) do
+      loggin_principal
+    end
+
+    describe "with valid params" do
+      it "put update" do
+        teacher = Teacher.create!(valid_inputs)
+        put :update, {id: teacher.to_param, teacher: valid_inputs_update}
+        teacher.reload
+      end
+      
+      it "redirects to @teacher" do
+        teacher = Teacher.create!(valid_inputs)
+        put :update, {id: teacher.to_param, teacher: valid_inputs_update}
+        expect(assigns(:teacher)).to eq(teacher)
+        expect(response).to render_template('edit')
+      end
+    end
+
+    describe "with invalid params" do
+      it "does not update" do
+        teacher = Teacher.create!(valid_inputs)
+        allow_any_instance_of(Teacher).to receive(:save).and_return(false)
+        put :update, {id: teacher.to_param, teacher: invalid_inputs}
+        expect(assigns(:teacher)).to eq(teacher)
+      end
+
+      it "re-render edit template" do
+        teacher = Teacher.create!(valid_inputs)
+        allow_any_instance_of(Teacher).to receive(:save).and_return(false)
+        put :update, {id: teacher.to_param, teacher: invalid_inputs}
+        expect(assigns(:teacher)).to eq(teacher)
+        expect(response).to render_template('edit')
       end
     end
   end
