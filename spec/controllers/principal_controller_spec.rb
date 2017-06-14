0583a59514040da5533ae609bcab3276bf81b878
FilipeKN4 <filipekn4@gmail.com>
2017-06-13 22:21:39 -0300

    Performs tests for update method in principal controller.
    Signed-off-by: FilipeKN4 <filipekn4@gmail.com>
    Signed-off-by: André de Sousa Costa filho <dedaoskater@hotmail.com>

--- spec/controllers/principal_controller_spec.rb
+++ spec/controllers/principal_controller_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe PrincipalController, type: :controller do
                          address: "samambaia Norte", employee_cpf: "26184391208",
                          registry:"123456",} }
 
+  let(:invalid_inputs) { { name: "Michael Cera", phone:"61988885555",
+                         password: "12345678", gender:"M",
+                         birth_date:"07/06/1970", shift: "",
+                         admission_date: "08/09/2007",
+                         address: "samambaia Norte", employee_cpf: "",
+                         registry:"1234",} }
+
+
   describe 'GET show' do
     describe "with right permissions" do
       before(:each) do
@@ -20,13 +28,13 @@ RSpec.describe PrincipalController, type: :controller do
       end
 
       it 'assigns the requested principal to @principal' do
-        principal = Employee.create! valid_inputs
+        principal = Principal.create! valid_inputs
         get :show, params:{id: principal.id}
         expect(assigns(:principal)).to eq(principal)
       end
 
       it 'render the show template' do
-        principal = Employee.create! valid_inputs
+        principal = Principal.create! valid_inputs
         get :show, params:{id: principal.id}
         expect(response).to render_template('show')
       end
@@ -40,15 +48,55 @@ RSpec.describe PrincipalController, type: :controller do
       end
 
       it 'assigns the requested principal to @principal' do
-        principal = Employee.create! valid_inputs
+        principal = Principal.create! valid_inputs
         get :edit, params:{id: principal.id}
         expect(assigns(:principal)).to eq(principal)
       end
 
       it 'render the edit template' do
-        principal = Employee.create! valid_inputs
+        principal = Principal.create! valid_inputs
         get :edit, params:{id: principal.id}
         expect(response).to render_template('edit')
+      end
+    end
+  end
+
+  describe 'PUT update' do
+    before(:each) do
+      login_principal
+    end
+
+    describe 'with valid inputs' do
+      it 'assigns the requested principal to @principal' do
+        principal = Principal.create! valid_inputs
+        put :update, params:{id: principal.id, principal: valid_inputs}
+        expect(assigns(:principal)).to eq(principal)
+      end
+
+      it 'updates the requested principal' do
+        principal = Principal.create! valid_inputs
+        put :update, params:{id: principal.id, principal: valid_inputs}
+        principal.reload
+      end
+
+      it 'redirects to the principal' do
+        principal = Principal.create! valid_inputs
+        put :update, params:{id: principal.id, principal: valid_inputs}
+        expect(response).to redirect_to principal_path(assigns(:principal))
+      end
+    end
+
+    describe 'with invalid inputs' do
+      it 'assigns the requested principal to @principal' do
+        principal = Principal.create! valid_inputs
+        put :update, params:{id: principal.id, principal: invalid_inputs}
+        expect(assigns(:principal)).to eq(principal)
+      end
+
+      it 'does not update' do
+        principal = Principal.create! valid_inputs
+        put :update, params:{id: principal.id, principal: invalid_inputs}
+        principal.reload
       end
     end
   end
