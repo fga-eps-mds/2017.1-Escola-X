541e11e89b0a39f9f1f00e58cb8c8f2110f547e2
André de Sousa Costa filho <dedaoskater@hotmail.com>
2017-06-23 17:26:59 -0300

    Makes seed aand removes validations.
    Signed-off-by: André de Sousa Costa filho <dedaoskater@hotmail.com>
    Signed-off-by Lucas Mattiolli <lucas.mattioli@live.com>

--- app/models/alumn.rb
+++ app/models/alumn.rb
@@ -37,30 +37,11 @@ class Alumn < ApplicationRecord
                        :too_short => "deve possuir no mínimo 5 caracteres",
                        :too_long => "deve possuir no máximo 6 caracteres" }
 
-  validates :shift, presence: { message: "não pode estar em branco" },
-            length: { minimum: 7,
-                      maximum: 11,
-                      :too_short => "deve possuir no mínimo 7 caracteres",
-                      :too_long => "deve possuir no máximo 11 caracteres" }
-
-
-  validates :birth_date, presence: { message: "Não pode estar em branco." }
-
   validates :name, presence: { message: "não pode estar em branco" },
             length: { minimum: 5,
                       maximum: 64,
                       :too_short => "deve possuir no mínimo 5 caracteres",
                       :too_long => "deve possuir no máximo 64 caracteres" }
-
-  validates :address, presence: { message: "não pode estar em branco" },
-            length: { minimum: 5,
-                      maximum: 64,
-                      :too_short => "deve possuir no mínimo 5 caracteres",
-                      :too_long => "deve possuir no máximo 64 caracteres" }
-
-  validates :phone, length: { in: 10..11,
-                              :too_short => "deve possuir no mínimo 10 dígitos",
-                              :too_long => "deve possuir no máximo 11 dígitos" }
 
   validates :gender, presence: { message: "Não pode estar em branco." }
 
--- app/models/parent.rb
+++ app/models/parent.rb
@@ -7,26 +7,12 @@ class Parent < ApplicationRecord
   has_secure_password
   before_save :validates_password
 
-
-  validates :birth_date, presence: { message: "Não pode estar em branco." }
-
   validates :name, presence: { message: "não pode estar em branco" },
             length: { minimum: 5,
                       maximum: 64,
                      :too_short => "deve possuir no mínimo 5 caracteres",
                      :too_long => "deve possuir no máximo 64 caracteres" }
 
-  validates :address, presence: { message: "não pode estar em branco" },
-             length: { minimum: 5,
-                       maximum: 64,
-                       :too_short => "deve possuir no mínimo 5 caracteres",
-                       :too_long => "deve possuir no máximo 64 caracteres" }
-
-  validates :phone, length: { in: 10..11,
-                              :too_short => "deve possuir no mínimo 10 dígitos",
-                              :too_long => "deve possuir no máximo 11 dígitos" }
-
-  validates :gender, presence: { message: "Não pode estar em branco." }
 
   before_create{
     generate_token(:authorization_token)
