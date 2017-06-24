9cc243cd0498e1afd59bc6a011623f72c8314030
matheusbsilva <matheusbattista@hotmail.com>
2017-06-24 13:31:06 +0000

    Serialize created_at of notification, strike, suspension and alumn.

--- app/serializers/suspension_serializer.rb
+++ app/serializers/suspension_serializer.rb
@@ -1,4 +1,4 @@
 class SuspensionSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :date_suspension, :quantity_days
+  attributes :id, :title, :description, :date_suspension, :quantity_days, :created_at
   has_one :alumn
 end
